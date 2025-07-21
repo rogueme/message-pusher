3acd08bb1546ae27006921750eaaa414592b9196
rogueme <37013377+rogueme@users.noreply.github.com>
2025-07-21 11:24:27 +0800

    Update message.go

--- model/message.go
+++ model/message.go
@@ -1,227 +1,136 @@
 package model
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
-	"fmt"
 	"message-pusher/common"
-	"message-pusher/model"
-	"net/http"
-	"strings"
 	"time"
 )
 
-type wechatCorpAccountResponse struct {
-	ErrorCode    int    `json:"errcode"`
-	ErrorMessage string `json:"errmsg"`
-	AccessToken  string `json:"access_token"`
-	ExpiresIn    int    `json:"expires_in"`
+type Message struct {
+	Id          int       `json:"id"`
+	UserId      int       `json:"user_id" gorm:"index"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Content     string    `json:"content"`
+	URL         string    `json:"url" gorm:"column:url"`
+	Btntxt      string    `json:"btntxt"`
+	Channel     string    `json:"channel"`
+	Token       string    `json:"token" gorm:"-:all"`
+	HTMLContent string    `json:"html_content"  gorm:"-:all"`
+	Timestamp   int64     `json:"timestamp" gorm:"type:bigint"`
+	Link        string    `json:"link" gorm:"unique;index"`
+	To          string    `json:"to" gorm:"column:to"`           // if specified, will send to this user(s)
+	Status      int       `json:"status" gorm:"default:0;index"` // pending, sent, failed
+	OpenId      string    `json:"openid" gorm:"-:all"`           // alias for to
+	Desp        string    `json:"desp" gorm:"-:all"`             // alias for content
+	Short       string    `json:"short" gorm:"-:all"`            // alias for description
+	Async       bool      `json:"async" gorm:"-"`                // if true, will send message asynchronously
+	RenderMode  string    `json:"render_mode" gorm:"raw"`        // markdown (default), code, raw
+	Articles    []Article `gorm:"type:json;serializer:json"`     // 通用文章列表，支持 news 和 mpnews 消息类型
 }
 
-type WeChatCorpAccountTokenStoreItem struct {
-	CorpId      string
-	AgentSecret string
-	AgentId     string
-	AccessToken string
+type Article struct {
+	// news 消息字段
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	URL         string `json:"url"`
+	PicURL      string `json:"picurl"`
+
+	// mpnews 消息字段
+	ThumbMediaID     string `json:"thumb_media_id"`
+	Author           string `json:"author"`
+	ContentSourceURL string `json:"content_source_url"`
+	Content          string `json:"content"`
+	Digest           string `json:"digest"`
 }
 
-func (i *WeChatCorpAccountTokenStoreItem) Key() string {
-	return i.CorpId + i.AgentId + i.AgentSecret
-}
-
-func (i *WeChatCorpAccountTokenStoreItem) IsShared() bool {
-	appId := fmt.Sprintf("%s|%s", i.CorpId, i.AgentId)
-	var count int64 = 0
-	model.DB.Model(&model.Channel{}).Where("secret = ? and app_id = ? and type = ?",
-		i.AgentSecret, appId, model.TypeWeChatCorpAccount).Count(&count)
-	return count > 1
-}
-
-func (i *WeChatCorpAccountTokenStoreItem) IsFilled() bool {
-	return i.CorpId != "" && i.AgentSecret != "" && i.AgentId != ""
-}
-
-func (i *WeChatCorpAccountTokenStoreItem) Token() string {
-	return i.AccessToken
-}
-
-func (i *WeChatCorpAccountTokenStoreItem) Refresh() {
-	// https://work.weixin.qq.com/api/doc/90000/90135/91039
-	client := http.Client{
-		Timeout: 5 * time.Second,
+func GetMessageByIds(id int, userId int) (*Message, error) {
+	if id == 0 || userId == 0 {
+		return nil, errors.New("id 或 userId 为空！")
 	}
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s",
-		i.CorpId, i.AgentSecret), nil)
-	if err != nil {
-		common.SysError(err.Error())
-		return
-	}
-	responseData, err := client.Do(req)
-	if err != nil {
-		common.SysError("failed to refresh access token: " + err.Error())
-		return
-	}
-	defer responseData.Body.Close()
-	var res wechatCorpAccountResponse
-	err = json.NewDecoder(responseData.Body).Decode(&res)
-	if err != nil {
-		common.SysError("failed to decode wechatCorpAccountResponse: " + err.Error())
-		return
-	}
-	if res.ErrorCode != 0 {
-		common.SysError(res.ErrorMessage)
-		return
-	}
-	i.AccessToken = res.AccessToken
-	common.SysLog("access token refreshed")
+	message := Message{Id: id, UserId: userId}
+	err := DB.Where(message).First(&message).Error
+	return &message, err
 }
 
-type wechatCorpMessageRequest struct {
-	MessageType string `json:"msgtype"`
-	ToUser      string `json:"touser"`
-	AgentId     string `json:"agentid"`
-	TextCard    struct {
-		Title       string `json:"title"`
-		Description string `json:"description"`
-		URL         string `json:"url"`
-		Btntxt      string `json:"btntxt"`
-	} `json:"textcard"`
-	Text struct {
-		Content string `json:"content"`
-	} `json:"text"`
-	Markdown struct {
-		Content string `json:"content"`
-	} `json:"markdown"`
-	News struct {
-		Articles []struct {
-			Title       string `json:"title"`
-			Description string `json:"description"`
-			URL         string `json:"url"`
-			PicURL      string `json:"picurl"`
-		} `json:"articles"`
-	} `json:"news"`
-	MpNews struct {
-		Articles []struct {
-			Title            string `json:"title"`
-			ThumbMediaID     string `json:"thumb_media_id"`
-			Author           string `json:"author"`
-			ContentSourceURL string `json:"content_source_url"`
-			Content          string `json:"content"`
-			Digest           string `json:"digest"`
-		} `json:"articles"`
-	} `json:"mpnews"`
-}
-
-type wechatCorpMessageResponse struct {
-	ErrorCode    int    `json:"errcode"`
-	ErrorMessage string `json:"errmsg"`
-}
-
-func parseWechatCorpAccountAppId(appId string) (string, string, error) {
-	parts := strings.Split(appId, "|")
-	if len(parts) != 2 {
-		return "", "", errors.New("无效的微信企业号配置")
+func GetMessageById(id int) (*Message, error) {
+	if id == 0 {
+		return nil, errors.New("id 为空！")
 	}
-	return parts[0], parts[1], nil
+	message := Message{Id: id}
+	err := DB.Where(message).First(&message).Error
+	return &message, err
 }
 
-func SendWeChatCorpMessage(message *model.Message, user *model.User, channel_ *model.Channel) error {
-    if message == nil || user == nil || channel_ == nil {
-        return errors.New("message, user or channel is nil")
-    }
-	// https://developer.work.weixin.com/document/path/90236
-	corpId, agentId, err := parseWechatCorpAccountAppId(channel_.AppId)
+func GetAsyncPendingMessageIds() (ids []int, err error) {
+	err = DB.Model(&Message{}).Where("status = ?", common.MessageSendStatusAsyncPending).Pluck("id", &ids).Error
+	return ids, err
+}
+
+func GetMessageByLink(link string) (*Message, error) {
+	if link == "" {
+		return nil, errors.New("link 为空！")
+	}
+	message := Message{Link: link}
+	err := DB.Where(message).First(&message).Error
+	return &message, err
+}
+
+func GetMessageStatusByLink(link string) (int, error) {
+	if link == "" {
+		return common.MessageSendStatusUnknown, errors.New("link 为空！")
+	}
+	message := Message{}
+	err := DB.Where("link = ?", link).Select("status").First(&message).Error
+	return message.Status, err
+}
+
+func GetMessagesByUserId(userId int, startIdx int, num int) (messages []*Message, err error) {
+	err = DB.Select([]string{"id", "title", "channel", "timestamp", "status"}).
+		Where("user_id = ?", userId).Order("id desc").Limit(num).Offset(startIdx).Find(&messages).Error
+	return messages, err
+}
+
+func SearchMessages(keyword string) (messages []*Message, err error) {
+	err = DB.Select([]string{"id", "title", "channel", "timestamp", "status"}).
+		Where("id = ? or title LIKE ? or description LIKE ? or content LIKE ?", keyword, keyword+"%", keyword+"%", keyword+"%").
+		Order("id desc").
+		Find(&messages).Error
+	return messages, err
+}
+
+func DeleteMessageById(id int, userId int) (err error) {
+	// Why we need userId here? In case user want to delete other's message.
+	if id == 0 || userId == 0 {
+		return errors.New("id 或 userId 为空！")
+	}
+	message := Message{Id: id, UserId: userId}
+	err = DB.Where(message).First(&message).Error
 	if err != nil {
 		return err
 	}
-	userId := channel_.AccountId
-	clientType := channel_.Other
-	agentSecret := channel_.Secret
-	messageRequest := wechatCorpMessageRequest{
-		ToUser:  userId,
-		AgentId: agentId,
-	}
-	if message.To != "" {
-		messageRequest.ToUser = message.To
-	}
+	return message.Delete()
+}
 
-	if clientType == "plugin" {
-		// 按优先级和关键属性判断消息类型
-		if len(message.Articles) > 0 {
-			// 检查是否有 mpnews 所需的 content 字段来判断是否为 mpnews
-			if message.Articles[0].Content != "" && message.Articles[0].ThumbMediaID != "" {
-				messageRequest.MessageType = "mpnews"
-				for _, article := range message.Articles {
-					messageRequest.MpNews.Articles = append(messageRequest.MpNews.Articles, struct {
-						Title            string `json:"title"`
-						ThumbMediaID     string `json:"thumb_media_id"`
-						Author           string `json:"author"`
-						ContentSourceURL string `json:"content_source_url"`
-						Content          string `json:"content"`
-						Digest           string `json:"digest"`
-					}{ 
-						Title:            article.Title,
-						ThumbMediaID:     article.ThumbMediaID,
-						Author:           article.Author,
-						ContentSourceURL: article.ContentSourceURL,
-						Content:          article.Content,
-						Digest:           article.Digest,
-					})
-				}
-			} else {
-				// 否则判断为 news 类型
-				messageRequest.MessageType = "news"
-				for _, article := range message.Articles {
-					messageRequest.News.Articles = append(messageRequest.News.Articles, struct {
-						Title       string `json:"title"`
-						Description string `json:"description"`
-						URL         string `json:"url"`
-						PicURL      string `json:"picurl"`
-					}{
-						Title:       article.Title,
-						Description: article.Description,
-						URL:         article.URL,
-						PicURL:      article.PicURL,
-					})
-				}
-			}
-		} else if message.Title != "" {
-			// textcard 消息判断：存在 title 属性
-			messageRequest.MessageType = "textcard"
-			messageRequest.TextCard.Title = message.Title
-			messageRequest.TextCard.Description = message.Description
-			messageRequest.TextCard.URL = message.URL
-			messageRequest.TextCard.Btntxt = message.Btntxt
-		} else if message.Content != "" {
-			// text 消息判断：存在 content 属性
-			messageRequest.MessageType = "text"
-			messageRequest.Text.Content = message.Content
-		}
-	} else if message.Content != "" {
-		// 非 plugin 客户端，使用 markdown 类型
-		messageRequest.MessageType = "markdown"
-		messageRequest.Markdown.Content = message.Content
-	}
+func DeleteAllMessages() error {
+	return DB.Exec("DELETE FROM messages").Error
+}
 
-	jsonData, err := json.Marshal(messageRequest)
-	if err != nil {
-		return err
-	}
-	key := fmt.Sprintf("%s%s%s", corpId, agentId, agentSecret)
-	accessToken := TokenStoreGetToken(key)
-	resp, err := http.Post(fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s", accessToken), "application/json",
-		bytes.NewBuffer(jsonData))
-	if err != nil {
-		return err
-	}
-	var res wechatCorpMessageResponse
-	err = json.NewDecoder(resp.Body).Decode(&res)
-	if err != nil {
-		return err
-	}
-	if res.ErrorCode != 0 {
-		return errors.New(res.ErrorMessage)
-	}
-	return nil
+func (message *Message) UpdateAndInsert(userId int) error {
+	message.Timestamp = time.Now().Unix()
+	message.UserId = userId
+	message.Status = common.MessageSendStatusPending
+	var err error
+	err = DB.Create(message).Error
+	return err
+}
+
+func (message *Message) UpdateStatus(status int) error {
+	err := DB.Model(message).Update("status", status).Error
+	return err
+}
+
+func (message *Message) Delete() error {
+	err := DB.Delete(message).Error
+	return err
 }
